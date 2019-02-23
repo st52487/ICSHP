7c54a6672adc6781184adfb07d4ee0738d029c47
st52487 <43894020+st52487@users.noreply.github.com>
2019-02-23 18:04:04 +0100

    cv01 ulohy

---
+++ Cv1Solution/Greetings/Program.cs
@@ -0,0 +1,65 @@
+﻿/*
+ * 
+ * REZANINA.EMAIL@GMAIL.COM
+ * 
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Greetings
+{
+    class Program
+    {
+        static uint GetCount(string[] args)
+        {
+            uint DEFAULT_COUNT = 100;
+            if (args.Length == 0)
+            {
+                return DEFAULT_COUNT;
+            } else 
+            {
+                if (uint.TryParse(args[0], out uint temp))
+                {
+                    return temp;
+                }
+                else
+                {
+                    return DEFAULT_COUNT;
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.Write("Hell World");
+            Console.WriteLine("Again Hell World");
+
+            uint count = GetCount(args);
+            /*String greetings = "Hell World";
+            for (uint i = 0; i < count; i++)
+            {
+                greetings += "again ";
+            }
+            greetings += "!";
+            if (greetings.Length > 1)
+            {
+                StringBuilder sb = new StringBuilder(greetings);
+                sb.Remove(greetings.Length-2, 1);
+                greetings = sb.ToString();
+            }*/
+
+            StringBuilder greetings = new StringBuilder("Hell World");
+            greetings.Append(" again");
+            for (uint i = 0; i < count; i++)
+            {
+                greetings.Append(" again");
+            }
+            greetings.Append("!");
+            
+            Console.Write(greetings);
+        }
+    }
+}
---
+++ Cv1Solution/cv1_podleZadani/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cv1_podleZadani
+{
+    class Program
+    {
+        static void funkceRodneCislo()
+        {
+            Console.WriteLine("Příklad 3 RČ");
+            StringBuilder rcBuilder = new StringBuilder("911213/1234");
+            StringBuilder mesic = new StringBuilder();
+            mesic.Append(rcBuilder[2]);
+            mesic.Append(rcBuilder[3]);
+            if (int.Parse(mesic.ToString()) - 50 >= 0)
+            {
+                Console.WriteLine(rcBuilder.Append(" je žena"));
+            }
+            else
+            {
+                Console.WriteLine(rcBuilder.Append(" je muž"));
+            }
+            Console.WriteLine("");
+            Console.WriteLine("*********************");
+        }
+
+        static void Main(string[] args)
+        {
+            /*  pr 1 formatovani adresy */
+            Console.WriteLine("Příklad 1 adresa");
+            Console.Write("Josef");
+            Console.Write(" ");
+            Console.Write("Novák");
+            Console.Write('\n');
+            Console.WriteLine("Jindřišská 16");
+            Console.WriteLine("111 50, Praha 1");
+            Console.WriteLine("*********************");
+
+
+            /* pr 2 abeceda */
+            Console.WriteLine("Příklad 2 abeceda");
+            for (int i = 97; i < 97 + 26; i++)
+            {
+                Console.Write((char)i + " ");
+            }
+            Console.WriteLine("");
+            Console.WriteLine("*********************");
+
+            /* pr 3 Rč */
+            funkceRodneCislo();
+
+            /* pr 4 hadani cisel*/
+            Random rnd = new Random();
+            int cislo = rnd.Next(101);
+            int zadano;
+            int pocetPokusu = 10;
+            bool vyhral = true;
+            do
+            {
+                Console.WriteLine("Zadej tvuj odhad: ");
+                int.TryParse(Console.ReadLine(), out zadano);
+
+                if(zadano < cislo)
+                {
+                    Console.WriteLine("Cislo je moc male");
+                }
+                if(zadano > cislo)
+                {
+                    Console.WriteLine("Cislo je moc velke");
+                }
+                pocetPokusu--;
+                if(pocetPokusu == 0)
+                {
+                    Console.WriteLine("Prohral jsi!");
+         
+                    break;
+                }
+                if(zadano == cislo)
+                {
+                    vyhral = true;
+                }
+            } while (cislo != zadano);
+           
+        }
+    }
+}
