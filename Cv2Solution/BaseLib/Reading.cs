4dfe3c576d0b530d46f85ae1a7aac6930810d98f
st52487 <43894020+st52487@users.noreply.github.com>
2019-02-26 12:13:28 +0100

    vytvoreneUlohyZezadani castecne

---
+++ Cv2Solution/BaseLib/Reading.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fei
+{
+    namespace BaseLib
+    {
+        /// <summary>
+        ///  staticka trida reading
+        /// </summary>
+        public static class Reading
+        {
+            /// <summary>
+            /// Nacte double z klavesnice
+            /// </summary>
+            /// <returns>vraci double hodnotu</returns>
+            public static double ReadDouble()
+            {
+                Console.WriteLine("Nacti double: ");
+                return Double.Parse(Console.ReadLine());
+            }
+
+            /// <summary>
+            /// Nacte int z klavesnice
+            /// </summary>
+            /// <returns>Vraci int</returns>
+            public static int ReadInt()
+            {
+                Console.WriteLine("Nacti int: ");
+                return Convert.ToInt32(Console.ReadLine());
+            }
+
+            /// <summary>
+            /// Nacte cjar z klavesnice
+            /// </summary>
+            /// <returns>Vraci char</returns>
+            public static char ReadChar()
+            {
+                Console.WriteLine("Nacti char: ");
+                return Console.ReadKey().KeyChar;
+            }
+
+            /// <summary>
+            /// Nacte retezec z klavesnice
+            /// </summary>
+            /// <returns>Vraci string</returns>
+            public static string ReadString()
+            {
+                Console.WriteLine("Nacti string: ");
+                return Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ Cv2Solution/UlohyZadani/Program.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Fei.BaseLib;
+
+namespace UlohyZadani
+{
+
+
+    class Program
+    {
+
+        public static void WriteField(int[] field)
+        {
+            for (int i = 0; i < field.Length; i++)
+            {
+                Console.Write("Obsah pole je: " + field[i] + " ");
+            }
+        }
+
+        public static int GetMinimumValue(int[] field)
+        {
+            int minimum = Int32.MaxValue;
+
+            for (int i = 0; i < field.Length; i++)
+            {
+                if(field[i] < minimum)
+                {
+                    minimum = field[i];
+                }
+            }
+
+            return minimum;
+        }
+
+        public static int SearchFirstValue(int[] field, int value)
+        {
+            int index = 0;
+            for (int i = 0; i < field.Length; i++)
+            {
+                if(field[i] == value)
+                {
+                    break;
+                }
+                index++;
+            }
+
+            return index;
+        }
+
+        public static int? SearchLastValue(int[] field, int value)
+        {
+            int index = 0;
+            int? lastIndexSeen = null;
+
+            for (int i = 0; i < field.Length; i++)
+            {
+                if(field[i] == value)
+                {
+                    lastIndexSeen = index;
+                }
+                index++;
+            }
+
+            return lastIndexSeen;
+        }
+
+        public static int[] GetField()
+        {
+            List<int> list = new List<int>();
+            int[] field = null;
+            
+
+
+            return field;
+        }
+
+
+        static void Main(string[] args)
+        {
+            int[] pole = null;
+            int volba;
+            do
+            {
+                Console.WriteLine("Zadeh svoji volbu: ");
+                int.TryParse(Console.ReadLine(), out volba);
+
+                switch (volba)
+                {
+                    case 1:
+                        break;
+                    case 2:
+                        WriteField(pole);
+                        break;
+                    case 3:
+                        break;
+                    case 4:
+                        GetMinimumValue(pole);
+                        break;
+                    case 5:
+                        SearchFirstValue(pole, 5);
+                        break;
+                    case 6:
+                        SearchLastValue(pole, 5);
+                        break;
+                }
+            } while (volba != 7);
+        }
+    }
+}
