e1207f6dc81b3bb29888d158d920fdce8c472ed8
st52487 <43894020+st52487@users.noreply.github.com>
2019-03-04 19:18:27 +0100

    cviko 3 / posledni verze

---
+++ Cv3Solution/Cv3Exersice/ClassTest.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cv3Exersice
+{
+    internal class MyInteger
+    {
+        //public uint Value { get; set; }
+
+        public uint Value
+        {
+            get
+            {
+                return Value;
+            }
+            set
+            {
+                Value = value;
+            }
+        }
+
+        private static int GetMaxInt() { return int.MaxValue; }
+
+        public bool Signed { get; private set; }
+
+        public MyInteger()
+        {
+         
+        }
+
+        public MyInteger(uint value, bool signed)
+        {
+            Value = value;
+            Signed = signed;
+        }
+
+        public static implicit operator MyInteger(int v)
+        {
+            return new MyInteger() { Value = (uint)Math.Abs(v), Signed = v < 0 };
+        }
+    }
+    class ClassTest
+    {
+        public static void DoIt()
+        {
+            MyInteger myInteger = new MyInteger() { Value = 33 };
+            MyInteger myInteger2 = new MyInteger(33, true);
+
+
+            MyInteger myInteger3 = 125;
+        }
+
+    }
+}
